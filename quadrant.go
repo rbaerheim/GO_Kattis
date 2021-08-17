be575564d0f84bcbf99544d2069f74dc32bc5b3c
Roger <rgbhm@hotmail.com>
2021-08-17 17:51:06 +0200

    Solved Quadrant Selection

--- quadrant.go
+++ quadrant.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    var x int
-    var y int
-    fmt.Scanf("%d", &x)
-    fmt.Scanf("%d", &y)
+	var x int
+	var y int
+	fmt.Scanf("%d", &x)
+	fmt.Scanf("%d", &y)
 
-    if x > 0 && y > 0 {
-        fmt.Println(1)
-    } else if x < 0 && y > 0 {
-        fmt.Print(2)
-    } else if x < 0 && y < 0 {
-        fmt.Print(3)
-    } else {
-        fmt.Print(4)
-    }
+	if x > 0 && y > 0 {
+		fmt.Println(1)
+	} else if x < 0 && y > 0 {
+		fmt.Print(2)
+	} else if x < 0 && y < 0 {
+		fmt.Print(3)
+	} else {
+		fmt.Print(4)
+	}
 }
