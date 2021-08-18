9a649ea39bb8a86aaa5a253122692e8a7204d3b8
Roger <rgbhm@hotmail.com>
2021-08-18 22:45:45 +0200

    Solved Pot

---
+++ pot.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var numBer int
+	var total, n float64
+	var pow float64
+
+	fmt.Scanf("%d", &numBer)
+
+	for i := 1; i <= numBer; i++ {
+		fmt.Scanf("%v", &pow)
+
+		intPow := math.Mod(pow, 10)
+
+		pow = math.Floor(pow / 10)
+
+		power := math.Pow(pow, intPow)
+
+		n += 1.0
+
+		total += power
+	}
+	fmt.Println(int(math.Round(total)))
+}
