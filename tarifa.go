51dd2165c14b5a3ee348cefb1d2e2f996aed4dfe
Roger <rgbhm@hotmail.com>
2021-08-18 12:10:20 +0200

    Solved Tarifa

--- tarifa.go
+++ tarifa.go
@@ -5,9 +5,30 @@ import (
 )
 
 func main() {
-	var x int
-	var n int
+	var megabyteseachMonth int
+	var numberofMonths int
+	var usedMBthismonth int
 
-	fmt.Scanf("%d", &x)
-	fmt.Scanf("%d", &n)
+	fmt.Scanf("%d", &megabyteseachMonth)
+	fmt.Scanf("%d", &numberofMonths)
+
+	i := 0
+
+	totalMBleft := megabyteseachMonth
+
+	for i != numberofMonths {
+		totalMBleft += megabyteseachMonth
+		
+		fmt.Scanf("%d", &usedMBthismonth)
+
+		if totalMBleft < usedMBthismonth {
+			break
+		}
+
+		totalMBleft -= usedMBthismonth
+
+		i = i + 1
+	}
+
+	fmt.Println(totalMBleft)
 }
