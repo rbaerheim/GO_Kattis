e69137d7066cd6dab6444b3b6e11f8c9cda32b2a
Roger <rgbhm@hotmail.com>
2021-08-18 20:05:55 +0200

    Solved Solving for Carrots

---
+++ solvingforcarrots.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var contestNum, carrotNum int
+	var description string
+
+	fmt.Scanf("%d %d", &contestNum, &carrotNum)
+
+	for s := 1; s <= contestNum; s++ {
+		fmt.Scanf("%s", description)
+	}
+
+	fmt.Println(carrotNum)
+}
