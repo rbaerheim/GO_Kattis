bf9953b411ad0c77a2b7bb55ed6365035b12f470
Roger <rgbhm@hotmail.com>
2021-08-18 12:19:26 +0200

    Solved Take Two Stones

---
+++ taketwostones.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var stones int
+
+	fmt.Scanf("%d", &stones)
+
+	if stones%2 == 0 {
+		fmt.Println("Bob")
+	} else {
+		fmt.Println("Alice")
+	}
+}
