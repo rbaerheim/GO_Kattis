4b4b427d8b2e8f2ca014470dc982a06f53f2f374
Roger <rgbhm@hotmail.com>
2021-08-18 21:06:11 +0200

    Solved Rating Problems

---
+++ ratingproblems.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var totalJudges, allreadyRated, ratingValues, totalPoints, judgesNotrated int
+
+	fmt.Scanf("%d %d", &totalJudges, &allreadyRated)
+
+	judgesNotrated = totalJudges - allreadyRated
+
+	for i := 1; i <= allreadyRated; i++ {
+		fmt.Scanf("%d", &ratingValues)
+		totalPoints += ratingValues
+	}
+
+	minmax := judgesNotrated * 3
+
+	floatmin := float64(minmax)
+	floatmax := float64(minmax)
+	floattotal := float64(totalPoints)
+	floatjudges := float64(totalJudges)
+
+	f64min := (floattotal - floatmin) / floatjudges
+	f64max := (floattotal + floatmax) / floatjudges
+
+	fmt.Printf("%v %v", f64min, f64max)
+}
