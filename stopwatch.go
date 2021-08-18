3c58e85984689760398f813ee4faed494bea6461
Roger <rgbhm@hotmail.com>
2021-08-18 18:44:45 +0200

    Solved Stopwatch

---
+++ stopwatch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numButtonPresses int
+	var time int
+	var totaltime int
+
+	fmt.Scanf("%d", &numButtonPresses)
+
+	for s := 1; s <= numButtonPresses; s++ {
+		fmt.Scanf("%d", &time)
+		if s%2 != 0 {
+			totaltime -= time
+		} else {
+			totaltime += time
+		}
+
+	}
+
+	if numButtonPresses % 2 != 0 {
+		fmt.Println("still running")
+	} else {
+		fmt.Println(totaltime)
+	}
+}
