32bd6a47cbdaefd7df10d1da0004cb32d0c6f070
Roger <rgbhm@hotmail.com>
2021-08-18 19:51:13 +0200

    Solved Sort Two Numbers

---
+++ sorttwonumbers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1 int
+	var num2 int
+
+	fmt.Scanf("%d", &num1)
+	fmt.Scanf("%d", &num2)
+
+	if num1 > num2 {
+		fmt.Println(num2, num1)
+	} else {
+		fmt.Println(num1, num2)
+	}
+}
