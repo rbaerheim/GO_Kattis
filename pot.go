2b37b61d479c71f8e7dc7c610f7fd2efe863790b
Roger <rgbhm@hotmail.com>
2021-08-18 22:47:46 +0200

    Solved Pot

--- pot.go
+++ pot.go
@@ -7,22 +7,15 @@ import (
 
 func main() {
 	var numBer int
-	var total, n float64
-	var pow float64
+	var total, pow float64
 
 	fmt.Scanf("%d", &numBer)
 
 	for i := 1; i <= numBer; i++ {
 		fmt.Scanf("%v", &pow)
-
 		intPow := math.Mod(pow, 10)
-
 		pow = math.Floor(pow / 10)
-
 		power := math.Pow(pow, intPow)
-
-		n += 1.0
-
 		total += power
 	}
 	fmt.Println(int(math.Round(total)))
