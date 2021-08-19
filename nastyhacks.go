46ed84e27c753c2b8bcab6d7f74ff209abc2c5ce
Roger <rgbhm@hotmail.com>
2021-08-19 19:37:13 +0200

    Solved Nasty Hacks

---
+++ nastyhacks.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var cases, doNOTadvertise, doAdvertise, cost int
+
+	fmt.Scanf("%d", &cases)
+
+	for i := 1; i <= cases; i++ {
+		fmt.Scanf("%d %d %d", &doNOTadvertise, &doAdvertise, &cost)
+		if (doAdvertise - cost) > doNOTadvertise {
+			fmt.Println("advertise")
+		} else if (doAdvertise - cost) < doNOTadvertise {
+			fmt.Println("do not advertise")
+		} else {
+			fmt.Println("does not matter")
+		}
+	}
+}
