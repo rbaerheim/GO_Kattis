fb0ca3615fb2b776e5188a7eebe44073e7725b79
Roger <rgbhm@hotmail.com>
2021-08-20 00:00:42 +0200

    Startet solving graduation

---
+++ graduation.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var lines, columns, classes int
+	var student string
+	var studNums [][]string
+	var studNums2 []string
+
+	fmt.Scanf("%d %d %d", &lines, &columns, &classes)
+
+	for i := 1; i <= lines; i++ {
+		fmt.Scanf("%s", &student)
+	}
+}
