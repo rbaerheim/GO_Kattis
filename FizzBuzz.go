73a6c6898de34b761d19a39420ea0da1b58b0027
Roger <rgbhm@hotmail.com>
2021-08-19 10:58:16 +0200

    Solved FizzBuzz

---
+++ FizzBuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2, num3 int
+
+	fmt.Scanf("%d %d %d", &num1, &num2, &num3)
+
+	for i := 1; i <= num3; i++ {
+		if i%num1 == 0 && i%num2 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%num2 == 0 {
+			fmt.Println("Buzz")
+		} else if i%num1 == 0 {
+			fmt.Println("Fizz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
