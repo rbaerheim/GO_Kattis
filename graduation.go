8624abf7614187df98cf03bc04a695740164b2cd
Roger <rgbhm@hotmail.com>
2021-08-20 10:25:23 +0200

    Started solving graduation

--- graduation.go
+++ graduation.go
@@ -7,12 +7,12 @@ import (
 func main() {
 	var lines, columns, classes int
 	var student string
-	var studNums [][]string
-	var studNums2 []string
+	var studNums []string
 
 	fmt.Scanf("%d %d %d", &lines, &columns, &classes)
 
 	for i := 1; i <= lines; i++ {
 		fmt.Scanf("%s", &student)
+		studNums = append(studNums, student)
 	}
 }
