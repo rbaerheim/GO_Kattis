1761791de5ed6eac8478dbc35e9bba796ab83bfd
Roger <rgbhm@hotmail.com>
2021-08-20 22:35:39 +0200

    Solved Last Factorial Digit

---
+++ lastfactorialdigit.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var inputNum int
+	var factorial int
+
+	fact := 1
+
+	fmt.Scanf("%d", &inputNum)
+
+	for i := 1; i <= inputNum; i++ {
+		fmt.Scanf("%d", &factorial)
+		if factorial == 1 {
+			fact = 1
+			fmt.Println(fact)
+		} else {
+			for j := 1; j <= factorial; j++ {
+				fact *= j
+			}
+			facti := fmt.Sprint(fact)
+			lastdigit := facti[len(facti)-1:]
+			fmt.Println(lastdigit)
+			fact = 1
+		}
+	}
+}
