2347c9af53ef828468dc22835c701bbae1f78ef8
Roger <rgbhm@hotmail.com>
2021-08-21 17:51:44 +0200

    Solved Jumbo Javelin

---
+++ jumbojavelin.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numJavs, lenghtJavs, totalLenght int
+
+	fmt.Scanf("%d", &numJavs)
+
+	for i := 1; i <= numJavs; i++ {
+		fmt.Scanf("%d", &lenghtJavs)
+		if i == 1 {
+			totalLenght = lenghtJavs
+			continue
+		}
+		totalLenght += lenghtJavs - 1
+	}
+	fmt.Println(totalLenght)
+}
