11b18f2f3a0bca816ddbd7d30e215979607d6970
Roger <rgbhm@hotmail.com>
2021-08-21 19:20:25 +0200

    Solved Triple Texting

---
+++ tripletexting.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	_ "strings"
+)
+
+func main() {
+	var input string
+	// var wordList []string
+
+	fmt.Scanf("%s", &input)
+
+	lenghtAll := len(input)
+
+	lenghtOne := lenghtAll / 3
+
+	firstWord := input[:lenghtOne]
+	secondWord := input[lenghtOne:(lenghtOne + lenghtOne)]
+	thirdWord := input[(lenghtOne + lenghtOne):(lenghtOne + lenghtOne + lenghtOne)]
+
+	if firstWord == secondWord {
+		fmt.Println(firstWord)
+	} else if secondWord == thirdWord {
+		fmt.Println(secondWord)
+	} else if firstWord == thirdWord {
+		fmt.Println(thirdWord)
+	} else {
+		fmt.Println(firstWord)
+	}
+}
